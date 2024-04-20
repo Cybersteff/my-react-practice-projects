375def371753fa66acefeafc5d95b1d81b2a7222
cybersteff <stephenadegbite001@gmail.com>
2024-04-20 13:10:40 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import indexedDB from "indexedDB";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [bd, setDb] = useState(null);
+  const [store, setStore] = useState(null);
+
+  useEffect(() => {
+    const initDB = async () => {
+      const db = await indexedDB.createDatabase("TodoDB");
+      setDb(db);
+      const conn = await db.open();
+      const store = conn.createObjectsStore("Todo", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      setStore(store);
+      store.createIndex("title", "title", { unique: false });
+      const todos = await store.getAll();
+      setTodos(todos);
+    };
+    initDB;
+  }, []);
+
+  const handleSubmit = (event) => {
+    const submitHandler = async () => {
+      event.preventDefault();
+      const newTodoObj = { title: newTodo, completed: false };
+      store.put(newTodoObj);
+      setTodos([...todos, newTodoObj]);
+      setNewTodo("");
+    };
+    submitHandler;
+  };
+
+  const handleToggelCompleted = (id) => {
+    const completedTaskHandler = async () => {
+      const todo = await store.get(id);
+      todo.completed = !todo.completed;
+      store.put(todo);
+      setTodos(todos.map((todo) => (todo.id === id ? todo : todo)));
+    };
+    completedTaskHandler;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+          placeholder="Enter new todo item"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.title}
+              {console.log(todo.title)}
+            </span>
+            <button onClick={() => handleToggelCompleted(todo.id)}>
+              {todo.completed ? "Undo" : "Done"}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default TodoList;
